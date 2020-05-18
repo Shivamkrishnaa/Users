465287d4517e1a973290e563af6805fa7483738b
shivamkrishna <shivamkrishna96@gmail.com>
2020-05-18 06:41:02 +0530

    port fix

--- src/config/app.js
+++ src/config/app.js
@@ -1,8 +1,8 @@
 export default {
     name : process.env.APP_NAME || "ProjectName",
     log  : process.env.APP_LOG || "dev",
-    port : process.env.APP_PORT || 800,
+    port : process.env.PORT|| process.env.APP_PORT || 800,
     secret : process.env.APP_SECRET || 'NodeJSProject',
-    url : process.env.APP_URL || 'http://localhost',
-    secure : (process.env.APP_SECURE == 'true') || false,
+    url : "https://bit-user-management.herokuapp.com/"||  process.env.APP_URL || 'http://localhost',
+    secure : true|| (process.env.APP_SECURE == 'true') || false,
 }
